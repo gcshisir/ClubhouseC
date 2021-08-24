06aaae0480994a5701bd9ad76b3861559205d036
Shisir GC <shisir.gc44@gmail.com>
2021-08-24 14:58:41 +0545

    added others in room section in room screen

--- lib/screens/room_screen.dart
+++ lib/screens/room_screen.dart
@@ -126,6 +126,36 @@ class RoomScreen extends StatelessWidget {
                     .toList(),
               ),
             ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  'Others in room',
+                  style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                        color: Colors.grey[400],
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.all(20.0),
+              sliver: SliverGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 20.0,
+                childAspectRatio: 0.7,
+                children: room.others
+                    .map(
+                      (e) => RoomUserProfile(
+                        imageUrl: e.imageUrl,
+                        size: 66.0,
+                        name: e.givenName,
+                        isNew: Random().nextBool(),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
           ],
         ),
       ),
